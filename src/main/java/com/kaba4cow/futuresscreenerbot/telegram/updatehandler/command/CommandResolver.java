39ee1306b1fe5cf2e4b294d2b8bf654685ab404d
kaba4cow <kaba4cow@gmail.com>
2025-06-25 15:18:18 +0300

    Switched to json.org

--- src/main/java/com/kaba4cow/futuresscreenerbot/telegram/updatehandler/command/CommandResolver.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/telegram/updatehandler/command/CommandResolver.java
@@ -1,13 +1,14 @@
 package com.kaba4cow.futuresscreenerbot.telegram.updatehandler.command;
 
-import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
@@ -18,23 +19,22 @@ import lombok.extern.slf4j.Slf4j;
 public class CommandResolver {
 
 	private final Map<String, CommandIdentifier> commandToIdentifierMappings = new TreeMap<>();
+
 	private final Map<CommandIdentifier, String> identifierToCommandMappings = new TreeMap<>();
 
 	@SneakyThrows
 	@PostConstruct
 	public void loadCommands() {
 		log.info("Loading commands");
-		ObjectMapper mapper = new ObjectMapper();
-		try (InputStream input = getClass().getClassLoader().getResourceAsStream("commands.json")) {
-			TypeReference<Map<String, String>> reference = new TypeReference<>() {};
-			Map<String, String> raw = mapper.readValue(input, reference);
-			for (Map.Entry<String, String> entry : raw.entrySet()) {
-				CommandIdentifier identifier = CommandIdentifier.valueOf(entry.getKey());
-				String command = entry.getValue();
-				commandToIdentifierMappings.put(command, identifier);
-				identifierToCommandMappings.put(identifier, command);
-				log.info("Registered command mapping \"{}\" <-> {}", command, identifier);
-			}
+		URL resource = getClass().getClassLoader().getResource("commands.json");
+		Path path = Paths.get(resource.toURI());
+		JSONObject json = new JSONObject(Files.readString(path));
+		for (String key : json.keySet()) {
+			CommandIdentifier identifier = CommandIdentifier.valueOf(key);
+			String command = json.getString(key);
+			commandToIdentifierMappings.put(command, identifier);
+			identifierToCommandMappings.put(identifier, command);
+			log.info("Registered command mapping \"{}\" <-> {}", command, identifier);
 		}
 	}
 
