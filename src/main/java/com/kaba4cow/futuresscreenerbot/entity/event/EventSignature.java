b4211e69808f3117fd0067f46d533e69e4fb8901
kaba4cow <kaba4cow@gmail.com>
2025-06-28 11:29:26 +0300

    Added EventSignature class for later use

---
+++ src/main/java/com/kaba4cow/futuresscreenerbot/entity/event/EventSignature.java
@@ -0,0 +1,37 @@
+package com.kaba4cow.futuresscreenerbot.entity.event;
+
+import com.kaba4cow.futuresscreenerbot.tool.Symbol;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class EventSignature {
+
+	@Embedded
+	@AttributeOverrides({ //
+			@AttributeOverride(name = "baseAsset", column = @Column(name = "column_base_asset", updatable = false)), //
+			@AttributeOverride(name = "quoteAsset", column = @Column(name = "column_quote_asset", updatable = false)) //
+	})
+	private Symbol symbol;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "column_type", updatable = false)
+	private EventType type;
+
+}
