48e20fafd515e2325ee6c1c48d86b1b05c761a4e
kaba4cow <kaba4cow@gmail.com>
2025-06-28 21:55:24 +0300

    Created TimeTracker class

---
+++ src/main/java/com/kaba4cow/futuresscreenerbot/tool/TimeTracker.java
@@ -0,0 +1,47 @@
+package com.kaba4cow.futuresscreenerbot.tool;
+
+public class TimeTracker {
+
+	private long startTimeMillis;
+	private boolean started;
+
+	private long finishTimeMillis;
+	private boolean finished;
+
+	public TimeTracker() {
+		reset();
+	}
+
+	public TimeTracker reset() {
+		startTimeMillis = 0L;
+		started = false;
+		finishTimeMillis = 0L;
+		finished = false;
+		return this;
+	}
+
+	public TimeTracker start() {
+		if (!finished)
+			throw new IllegalStateException("TimeTracker has not finished yet");
+		if (started)
+			throw new IllegalStateException("TimeTracker has already started");
+		startTimeMillis = System.currentTimeMillis();
+		started = true;
+		return this;
+	}
+
+	public TimeTracker finish() {
+		if (!started)
+			throw new IllegalStateException("TimeTracker has not started yet");
+		if (finished)
+			throw new IllegalStateException("TimeTracker has already finished");
+		finishTimeMillis = System.currentTimeMillis();
+		finished = true;
+		return this;
+	}
+
+	public long getDurationMillis() {
+		return finishTimeMillis - startTimeMillis;
+	}
+
+}
