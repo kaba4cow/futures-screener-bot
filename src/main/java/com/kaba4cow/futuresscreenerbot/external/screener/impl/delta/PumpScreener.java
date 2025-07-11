f96f6fe6d3ee6a0f597181439f2d5f87532a7e98
kaba4cow <kaba4cow@gmail.com>
2025-07-11 20:25:45 +0300

    Moved update logic from impls to DeltaScreener

--- src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/delta/PumpScreener.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/delta/PumpScreener.java
@@ -1,42 +1,17 @@
 package com.kaba4cow.futuresscreenerbot.external.screener.impl.delta;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.kaba4cow.futuresscreenerbot.config.properties.screener.settings.PumpScreenerSettingsProperties;
 import com.kaba4cow.futuresscreenerbot.entity.event.EventType;
-import com.kaba4cow.futuresscreenerbot.tool.Symbol;
-import com.kaba4cow.futuresscreenerbot.tool.barseries.Bar;
-import com.kaba4cow.futuresscreenerbot.tool.barseries.BarSeries;
 import com.kaba4cow.futuresscreenerbot.tool.util.MathUtil;
 
 @Component
 public class PumpScreener extends DeltaScreener<PumpScreenerSettingsProperties> {
 
-	private final Map<Symbol, BarSeries> map = new ConcurrentHashMap<>();
-
 	@Override
-	public void update(Symbol symbol, JSONObject jsonData) {
-		Bar newBar = new Bar(jsonData);
-		BarSeries barSeries = getBarSeries(symbol);
-		if (barSeries.addBar(newBar) && barSeries.getBarCount() == barSeries.getMaxBarCount()) {
-			Bar firstBar = barSeries.getFirst();
-			Bar lastBar = barSeries.getLast();
-			float firstPrice = firstBar.getClosePrice();
-			float lastPrice = lastBar.getClosePrice();
-			if (lastPrice > firstPrice) {
-				double deltaPrice = MathUtil.calculateDelta(firstPrice, lastPrice);
-				if (deltaPrice >= getSettingsThreshold())
-					registerEvent(symbol, deltaPrice);
-			}
-		}
-	}
-
-	private BarSeries getBarSeries(Symbol symbol) {
-		return map.computeIfAbsent(symbol, key -> new BarSeries(2));
+	protected double calculateDelta(double firstPrice, double lastPrice) {
+		return MathUtil.calculateDelta(firstPrice, lastPrice);
 	}
 
 	@Override
