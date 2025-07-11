5c96f740b5f7a1c962a806f04b9c6e07b983d851
kaba4cow <kaba4cow@gmail.com>
2025-07-11 19:05:47 +0300

    Added @Component to ScreenerStream impls

--- src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/stream/KLineScreenerStream.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/stream/KLineScreenerStream.java
@@ -1,5 +1,8 @@
 package com.kaba4cow.futuresscreenerbot.external.screener.stream;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class KLineScreenerStream implements ScreenerStream {
 
 	@Override
