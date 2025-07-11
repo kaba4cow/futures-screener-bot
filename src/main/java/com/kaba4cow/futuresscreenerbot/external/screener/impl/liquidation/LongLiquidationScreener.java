02082ff713687c3b5f045c46b1935db528815189
kaba4cow <kaba4cow@gmail.com>
2025-07-11 20:18:33 +0300

    Added abstract LiquidationScreener

--- src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/liquidation/LongLiquidationScreener.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/liquidation/LongLiquidationScreener.java
@@ -1,28 +1,16 @@
 package com.kaba4cow.futuresscreenerbot.external.screener.impl.liquidation;
 
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.kaba4cow.futuresscreenerbot.config.properties.screener.settings.LongLiquidationScreenerSettingsProperties;
 import com.kaba4cow.futuresscreenerbot.entity.event.EventType;
-import com.kaba4cow.futuresscreenerbot.external.screener.impl.AbstractScreener;
-import com.kaba4cow.futuresscreenerbot.external.screener.stream.impl.ForceOrderScreenerStream;
-import com.kaba4cow.futuresscreenerbot.tool.Symbol;
 
 @Component
-public class LongLiquidationScreener
-		extends AbstractScreener<LongLiquidationScreenerSettingsProperties, ForceOrderScreenerStream> {
+public class LongLiquidationScreener extends LiquidationScreener<LongLiquidationScreenerSettingsProperties> {
 
 	@Override
-	public void update(Symbol symbol, JSONObject jsonData) {
-		String side = jsonData.getJSONObject("o").getString("S");
-		if (side.equals("SELL")) {
-			float price = jsonData.getJSONObject("o").getFloat("p");
-			float quantity = jsonData.getJSONObject("o").getFloat("q");
-			double liquidation = price * quantity;
-			if (liquidation >= getSettingsThreshold())
-				registerEvent(symbol, liquidation);
-		}
+	protected String getTargetSide() {
+		return "SELL";
 	}
 
 	@Override
--- src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/liquidation/ShortLiquidationScreener.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/external/screener/impl/liquidation/ShortLiquidationScreener.java
@@ -1,28 +1,16 @@
 package com.kaba4cow.futuresscreenerbot.external.screener.impl.liquidation;
 
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.kaba4cow.futuresscreenerbot.config.properties.screener.settings.ShortLiquidationScreenerSettingsProperties;
 import com.kaba4cow.futuresscreenerbot.entity.event.EventType;
-import com.kaba4cow.futuresscreenerbot.external.screener.impl.AbstractScreener;
-import com.kaba4cow.futuresscreenerbot.external.screener.stream.impl.ForceOrderScreenerStream;
-import com.kaba4cow.futuresscreenerbot.tool.Symbol;
 
 @Component
-public class ShortLiquidationScreener
-		extends AbstractScreener<ShortLiquidationScreenerSettingsProperties, ForceOrderScreenerStream> {
+public class ShortLiquidationScreener extends LiquidationScreener<ShortLiquidationScreenerSettingsProperties> {
 
 	@Override
-	public void update(Symbol symbol, JSONObject jsonData) {
-		String side = jsonData.getJSONObject("o").getString("S");
-		if (side.equals("BUY")) {
-			float price = jsonData.getJSONObject("o").getFloat("p");
-			float quantity = jsonData.getJSONObject("o").getFloat("q");
-			double liquidation = price * quantity;
-			if (liquidation >= getSettingsThreshold())
-				registerEvent(symbol, liquidation);
-		}
+	protected String getTargetSide() {
+		return "BUY";
 	}
 
 	@Override
