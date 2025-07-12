69e2e72ea9174adc813e96af996ffbed69a6d8b2
kaba4cow <kaba4cow@gmail.com>
2025-07-12 17:42:45 +0300

    Added timeUnit parameter to WithProfiling

--- src/main/java/com/kaba4cow/futuresscreenerbot/aspect/WithProfiling.java
+++ src/main/java/com/kaba4cow/futuresscreenerbot/aspect/WithProfiling.java
@@ -6,10 +6,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 
 @Documented
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface WithProfiling {
+
+	TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
 
 }
